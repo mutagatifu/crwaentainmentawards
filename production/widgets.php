4584f73c242df633fff8a440c7e7ce7b38b2998d
“Hodard <hodardhazwinayo@gmail.com>
2018-09-11 14:03:30 +0200

    ajax validation

--- production/widgets.php
+++ production/widgets.php
@@ -86,19 +86,36 @@
                                         <div class="col-md-6 col-sm-6 col-xs-12">
                                           <div class="form-group">
                                             <select class="form-control" name="category">
-                                              <option value="Music" onclick="loadDoc()"> Music </option>
-                                              <option value="Comedy"> Comedy </option>
-                                              <option value="Dance"> Dance </option>
-                                              <option value="Fashion"> Fashion </option>
-                                                
+                                                <option value="Music"> Music </option>
+                                                <option value="Fashion"> Fashion </option>
+                                                <option value="Dance"> Dance </option>
+                                                <option value="Media"> Media </option>
+
                                             </select>
                                           </div>
                                         </div>
                                       </div>
+                                        <div class="item form-group">
+                                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="occupation">SubCategory <span class="required">*</span>
+                                            </label>
+                                            <div class="col-md-6 col-sm-6 col-xs-12">
+                                                <div class="form-group" id="category">
+                                                    <select class="form-control" name="category" multiple size="5">
+                                                        <option value="Music"> 1 </option>
+                                                        <option value="Music"> 2 </option>
+                                                        <option value="Music"> 3 </option>
+                                                        <option value="Music"> 4 </option>
+                                                        <option value="Music"> 5 </option>
+
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+
                                       <div class="item form-group">
                                         <label for="password" class="control-label col-md-3">Email</label>
                                         <div class="col-md-6 col-sm-6 col-xs-12">
-                                          <input id="password" type="password" name="password" data-validate-length="6,8" class="form-control col-md-7 col-xs-12" required="required">
+                                          <input id="password" type="email" name="email" data-validate-length="6,8" class="form-control col-md-7 col-xs-12" required="required">
                                         </div>
                                       </div>
                                       <div class="item form-group">
@@ -119,7 +136,20 @@
                                       <div class="form-group">
                                         <div class="col-md-6 col-md-offset-3">
                                           <button type="submit" class="btn btn-primary">Cancel</button>
-                                          <button id="send" type="submit" class="btn btn-success">Submit</button>
+                                          <button id="send" type="submit" onclick="loadDoc()" class="btn btn-success" >Submit</button>
+                                            <script>
+                                                function loadDoc() {
+                                                    var xhttp = new XMLHttpRequest();
+                                                    xhttp.onreadystatechange = function() {
+                                                        if (this.readyState == 4 && this.status == 200) {
+                                                            document.getElementById("category").innerHTML =
+                                                                this.responseText;
+                                                        }
+                                                    };
+                                                    xhttp.open("GET", "ajax_info.txt", true);
+                                                    xhttp.send();
+                                                }
+                                            </script>
                                         </div>
                                       </div>
                                         <script>
